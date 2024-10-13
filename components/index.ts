c29e2499c7f51dbfdcad0b2e483c464c38285677
Arjunkumar1877 <arjun.tech177@gmail.com>
2024-10-13 23:14:51 +0530

    footer section  designed

--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,12 @@
 import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export {
     Hero,
 CustomButton,
-Navbar
+Navbar,
+Footer
 
 }
